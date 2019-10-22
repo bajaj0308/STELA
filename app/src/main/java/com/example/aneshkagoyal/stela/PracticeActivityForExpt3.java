9e760cacb9e5fd85de4ba5a6179d3b9a6d308565
Aneshka Goyal <aneshkagoyal@gmail.com>
2019-10-22 20:54:58 +0530

    Firebase connectivity of DHT11 from app

--- app/src/main/java/com/example/aneshkagoyal/stela/PracticeActivityForExpt3.java
+++ app/src/main/java/com/example/aneshkagoyal/stela/PracticeActivityForExpt3.java
@@ -1,5 +1,6 @@
 package com.example.aneshkagoyal.stela;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PracticeActivityForExpt3 extends AppCompatActivity {
 
@@ -21,6 +31,9 @@ public class PracticeActivityForExpt3 extends AppCompatActivity {
     int marks=0;
     Button q1_s, q2_s, q3_s, q4_s, q5_s;
     int q1_f=0,q2_f=0,q3_f=0,q4_f=0,q5_f=0;
+    DatabaseReference ref,ref2,ref3;
+    Object device_num;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +48,28 @@ public class PracticeActivityForExpt3 extends AppCompatActivity {
         q3_s = (Button) findViewById(R.id.Q3_submit);
         q4_s = (Button) findViewById(R.id.Q4_submit);
         q5_s = (Button) findViewById(R.id.Q5_submit);
+        ref = FirebaseDatabase.getInstance().getReference().child("Student").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        ref2 = FirebaseDatabase.getInstance().getReference().child("Rpi");
+        final Map mp = new HashMap<String,Object>();
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChild("Device"))
+                    device_num = dataSnapshot.child("Device").getValue().toString();
+                else{
+                    Toast.makeText(PracticeActivityForExpt3.this,"Please go to dashboard to register",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(PracticeActivityForExpt3.this,Student_dashboard.class));
+                    finish();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         q1_s.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -140,6 +175,46 @@ public class PracticeActivityForExpt3 extends AppCompatActivity {
                 //TODO : put the experiment number on firebase
 
                 if(q1_f==1&&q2_f==1&&q3_f==1&&q4_f==1&&q5_f==1) {
+                    mp.put(device_num, num);
+                    ref2.updateChildren(mp).addOnCompleteListener(new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(PracticeActivityForExpt3.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                    ref3 = FirebaseDatabase.getInstance().getReference().child("Rpi").child(device_num.toString());
+                    ref3.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String temp_humid = dataSnapshot.getValue().toString();
+                            Log.d("val_of_temp",temp_humid);
+//                            while(temp_humid.equals(num)){
+//                                temp_humid = dataSnapshot.getValue().toString();
+//
+//                            }
+                            if(!temp_humid.equals(num) && !temp_humid.equals("0")){
+                                Toast.makeText(PracticeActivityForExpt3.this,"temperature and humidity values are :"+temp_humid,Toast.LENGTH_SHORT).show();
+                                mp.put(device_num, "0");
+                                ref2.updateChildren(mp);
+
+
+                            }
+
+
+
+
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
                     //TODO: update data on firebase and edit the following toast message
                     Toast.makeText(PracticeActivityForExpt3.this,"Submitted successfully",Toast.LENGTH_SHORT).show();
 
