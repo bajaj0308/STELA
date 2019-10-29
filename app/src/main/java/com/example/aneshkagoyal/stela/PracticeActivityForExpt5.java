6b921e942c8658754986aff5f15137c4d7cec8b2
Megha Bajaj <megha.bajaj@walmartlabs.com>
2019-10-30 00:45:26 +0530

    Backend for 2nd Experiment

---
+++ app/src/main/java/com/example/aneshkagoyal/stela/PracticeActivityForExpt5.java
@@ -0,0 +1,13 @@
+package com.example.aneshkagoyal.stela;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracticeActivityForExpt5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_for_expt_5);
+    }
+}
